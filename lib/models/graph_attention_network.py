9f2ee233451356ed7195a2120f5af280c21432de
Mrhonor <22210240239@m.fudan.edu.cn>
2023-05-10 16:01:27 +0800

    add alter arch train

--- lib/models/graph_attention_network.py
+++ lib/models/graph_attention_network.py
@@ -93,20 +93,20 @@ class GAT(nn.Module):
             cur_cat += self.dataset_cats[i]
             similar_matrix = torch.einsum('nc, mc -> nm', this_feats, unify_feats)
             softmax_similar_matrix = F.softmax(similar_matrix / 0.07, dim=1)
-            # softmax_similar_matrix[softmax_similar_matrix < self.threshold_value] = 0
-            # max_value, max_index = torch.max(softmax_similar_matrix, dim=0)
-            # bi_graph = torch.zeros(self.dataset_cats[i], self.max_num_unify_class)
-            # if x.is_cuda:
-            #     bi_graph = bi_graph.cuda()
+            softmax_similar_matrix[softmax_similar_matrix < self.threshold_value] = 0
+            max_value, max_index = torch.max(softmax_similar_matrix, dim=0)
+            bi_graph = torch.zeros(self.dataset_cats[i], self.max_num_unify_class)
+            if x.is_cuda:
+                bi_graph = bi_graph.cuda()
 
-            # bi_graph[max_index] = 1
+            bi_graph[max_index] = 1
             
             # this_iter_thresh = 0.3 + (self.threshold_value - 0.3) * self.configer.get('iter') / self.configer.get('lr', 'max_iter')
             # this_iter_thresh = self.threshold_value * self.configer.get('iter') / self.configer.get('lr', 'max_iter')
             # bi_graph[:, max_value < this_iter_thresh] = 0
             
             
-            self.bipartite_graphs.append(softmax_similar_matrix)
+            self.bipartite_graphs.append(bi_graph)
             
         return self.bipartite_graphsS
             
