0a55886fa56d4ced63114ef2a945d8739a38cfc0
Mrhonor <22210240239@m.fudan.edu.cn>
2023-11-07 17:36:28 +0800

    temp save

--- lib/models/resnet_pyramid.py
+++ lib/models/resnet_pyramid.py
@@ -19,6 +19,66 @@ model_urls = {
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
 
+class ConvBN(nn.Module):
+    ## ConvBNReLU类去掉ReLu层
+    def __init__(self, in_chan, out_chan, ks=3, stride=1, padding=1,
+                 dilation=1, groups=1, bias=False, n_bn=1):
+        ## n_bn bn层数量，对应混合的数据集数量
+        super(ConvBN, self).__init__()
+        self.conv = nn.Conv2d(
+                in_chan, out_chan, kernel_size=ks, stride=stride,
+                padding=padding, dilation=dilation,
+                groups=groups, bias=bias)
+        self.bn = nn.ModuleList([nn.BatchNorm2d(out_chan, affine=False) for i in range(0, n_bn)])
+        ## 采用共享的affine parameter
+        self.affine_weight = nn.Parameter(torch.empty(out_chan))
+        self.affine_bias = nn.Parameter(torch.empty(out_chan))
+        
+
+    def forward(self, x, dataset_id):
+        ## TODO 此处可以优化，不同数据集的图像过卷积层可以拼接到一起，过BN层再分离
+        feat = self.conv(x)
+        feat_list = [None] * len(dataset_id)
+        for i in set(dataset_id.cpu.numpy()):
+            feat_ = self.bn[i](x[dataset_id == i])
+            feat_ = feat_ * self.affine_weight.reshape(1,-1,1,1) + self.affine_bias.reshape(1,-1,1,1) 
+            feat_list[dataset_id == i] = feat_
+        feat_list = [feat[None] for feat in feat_list]
+        feat = torch.cat(feat_list, dim=0)
+        return feat
+        
+        # if len(other_x) != 0:
+        #     batch_size = [x.shape[0]]
+        #     for i in range(0, len(other_x)):
+        #         batch_size.append(other_x[i].shape[0])
+        #         x = torch.cat((x, other_x[i]), 0)
+        #     feat = self.conv(x)
+        #     feats = []
+        #     begin_index = 0
+        #     for i in range(0, len(other_x) + 1):
+        #         end_index = begin_index + batch_size[i]
+        #         feat_ = self.bn[i](feat[begin_index: end_index])
+                
+        #         ## affine param
+        #         feat_ = feat_ * self.affine_weight.reshape(1,-1,1,1) + self.affine_bias.reshape(1,-1,1,1)
+                
+        #         feats.append(feat_)
+        #         begin_index = end_index
+        # else:
+        #     feat = self.conv(x)
+        #     feat = self.bn[dataset](feat)
+            
+        #     ## affine param
+        #     feat = feat * self.affine_weight.reshape(1,-1,1,1) + self.affine_bias.reshape(1,-1,1,1)
+            
+        #     feats = [feat]
+        
+
+    def SetLastBNAttr(self, attr):
+        self.affine_weight.last_bn = attr
+        self.affine_bias.last_bn = attr
+        # for bn in self.bn:
+        #     bn.last_bn = attr
 
 def convkxk(in_planes, out_planes, stride=1, k=3):
     """kxk convolution with padding"""
@@ -34,6 +94,29 @@ def _bn_function_factory(conv, norm, relu=None):
 
     return bn_function
 
+def _mulbn_function_factory(conv, norm, affine_weight, affine_bias, relu=None):
+    def bn_function(x, dataset_id):
+        feat = conv(x)
+        feat_list = [None] * len(dataset_id)
+        for i in set(dataset_id.cpu.numpy()):
+            feat_ = norm[i](x[dataset_id == i])
+            feat_ = feat_ * affine_weight.reshape(1,-1,1,1) + affine_bias.reshape(1,-1,1,1) 
+            if relu is not None:
+                feat_ = relu(feat_)
+            
+            feat_list[dataset_id == i] = feat_
+        feat_list = [feat[None] for feat in feat_list]
+        feat = torch.cat(feat_list, dim=0)
+        
+        return feat
+        
+        # x = norm(conv(x))
+        # if relu is not None:
+        #     x = relu(x)
+        # return x
+
+    return bn_function
+
 
 def do_efficient_fwd(block, x, efficient):
     # return block(x)
@@ -41,6 +124,13 @@ def do_efficient_fwd(block, x, efficient):
         return cp.checkpoint(block, x)
     else:
         return block(x)
+    
+def do_efficient_fwd_mulbn(block, x, efficient, dataset_id):
+    # return block(x)
+    if efficient and x.requires_grad:
+        return cp.checkpoint(block, x, dataset_id)
+    else:
+        return block(x, dataset_id)
 
 
 class Identity(nn.Module):
@@ -78,6 +168,54 @@ class BasicBlock(nn.Module):
 
         if self.downsample is not None:
             residual = self.downsample(x)
+
+        out += residual
+        relu = self.relu(out)
+
+        return relu, out
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        super(BasicBlock, self)._load_from_state_dict(state_dict, prefix, local_metadata, False, missing_keys,
+                                                      unexpected_keys, error_msgs)
+        missing_keys = []
+        unexpected_keys = []
+        for bn in self.bn1:
+            bn._load_from_state_dict(state_dict, prefix + 'bn1.', local_metadata, strict, missing_keys, unexpected_keys,
+                                     error_msgs)
+        for bn in self.bn2:
+            bn._load_from_state_dict(state_dict, prefix + 'bn2.', local_metadata, strict, missing_keys, unexpected_keys,
+                                     error_msgs)
+                                    
+class MulBNBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, configer, inplanes, planes, stride=1, downsample=None, efficient=True, bn_class=nn.BatchNorm2d, levels=3):
+        super(MulBNBlock, self).__init__()
+        self.configer = configer
+        self.n_datasets = configer.get("n_datasets")
+        self.conv1 = convkxk(inplanes, planes, stride)
+        self.bn1 = nn.ModuleList([nn.ModuleList([bn_class(planes) for _ in range(levels)]) for _ in range(n_datasets)])
+        self.relu_inp = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
+        self.conv2 = convkxk(planes, planes)
+        self.bn2 = nn.ModuleList([nn.ModuleList([bn_class(planes) for _ in range(levels)]) for _ in range(n_datasets)])
+        self.downsample = downsample
+        self.stride = stride
+        self.efficient = efficient
+        self.num_levels = levels
+
+    def forward(self, x, level, dataset_id):
+        residual = x
+
+        bn_1 = _mulbn_function_factory(self.conv1, self.bn1[level], self.relu_inp)
+        bn_2 = _mulbn_function_factory(self.conv2, self.bn2[level])
+
+        out = do_efficient_fwd_mulbn(bn_1, x, self.efficient, dataset_id)
+        out = do_efficient_fwd_mulbn(bn_2, out, self.efficient, dataset_id)
+
+        if self.downsample is not None:
+            residual = self.downsample(x, dataset_id)
 
         out += residual
         relu = self.relu(out)
@@ -258,6 +396,167 @@ class ResNet(nn.Module):
             bn._load_from_state_dict(state_dict, prefix + 'bn1.', local_metadata, strict, missing_keys, unexpected_keys,
                                      error_msgs)
 
+class ResNet_mulbn(nn.Module):
+    def _make_layer(self, block, planes, blocks, stride=1, bn_class=nn.BatchNorm2d):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                bn_class(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.efficient, bn_class=bn_class,
+                            levels=self.pyramid_levels))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, bn_class=bn_class, levels=self.pyramid_levels, efficient=self.efficient))
+
+        return nn.Sequential(*layers)
+
+    def __init__(self, configer, block, layers, *, num_features=128, pyramid_levels=3, use_bn=True, k_bneck=1, k_upsample=3,
+                 efficient=False, upsample_skip=True, detach_upsample_skips=(), detach_upsample_in=False,
+                 align_corners=None, pyramid_subsample='bicubic', target_size=None,
+                 output_stride=4, **kwargs):
+        self.configer = configer
+        self.n_datasets = self.configer.get("n_datasets")
+        self.inplanes = 64
+        self.efficient = efficient
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        bn_class = nn.BatchNorm2d if use_bn else Identity
+        # self.register_buffer('img_mean', torch.tensor(mean).view(1, -1, 1, 1))
+        # self.register_buffer('img_std', torch.tensor(std).view(1, -1, 1, 1))
+        # if scale != 1:
+        #     self.register_buffer('img_scale', torch.tensor(scale).view(1, -1, 1, 1).float())
+
+        self.pyramid_levels = pyramid_levels
+        self.num_features = num_features
+        self.replicated = False
+
+        self.align_corners = align_corners
+        self.pyramid_subsample = pyramid_subsample
+
+        self.bn1 = nn.ModuleList([bn_class(64) for _ in range(pyramid_levels)])
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        bottlenecks = []
+        self.layer1 = self._make_layer(block, 64, layers[0], bn_class=bn_class)
+        bottlenecks += [convkxk(self.inplanes, num_features, k=k_bneck)]
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, bn_class=bn_class)
+        bottlenecks += [convkxk(self.inplanes, num_features, k=k_bneck)]
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, bn_class=bn_class)
+        bottlenecks += [convkxk(self.inplanes, num_features, k=k_bneck)]
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, bn_class=bn_class)
+        bottlenecks += [convkxk(self.inplanes, num_features, k=k_bneck)]
+
+        num_bn_remove = max(0, int(log2(output_stride) - 2))
+        self.num_skip_levels = self.pyramid_levels + 3 - num_bn_remove
+        bottlenecks = bottlenecks[num_bn_remove:]
+
+        self.fine_tune = [self.conv1, self.maxpool, self.layer1, self.layer2, self.layer3, self.layer4, self.bn1]
+
+        self.upsample_bottlenecks = nn.ModuleList(bottlenecks[::-1])
+        num_pyr_modules = 2 + pyramid_levels - num_bn_remove
+        self.target_size = target_size
+        if self.target_size is not None:
+            h, w = target_size
+            target_sizes = [(h // 2 ** i, w // 2 ** i) for i in range(2, 2 + num_pyr_modules)][::-1]
+        else:
+            target_sizes = [None] * num_pyr_modules
+        self.upsample_blends = nn.ModuleList(
+            [_UpsampleBlend(num_features,
+                            use_bn=use_bn,
+                            use_skip=upsample_skip,
+                            detach_skip=i in detach_upsample_skips,
+                            fixed_size=ts,
+                            k=k_upsample)
+             for i, ts in enumerate(target_sizes)])
+        self.detach_upsample_in = detach_upsample_in
+
+        self.random_init = [self.upsample_bottlenecks, self.upsample_blends]
+
+        self.features = num_features
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def random_init_params(self):
+        return chain(*[f.parameters() for f in self.random_init])
+
+    def fine_tune_params(self):
+        return chain(*[f.parameters() for f in self.fine_tune])
+
+    def forward_resblock(self, x, layers, idx):
+        skip = None
+        for l in layers:
+            x = l(x) if not isinstance(l, BasicBlock) else l(x, idx)
+            if isinstance(x, tuple):
+                x, skip = x
+        return x, skip
+
+    def forward_down(self, image, skips, idx=-1):
+        x = self.conv1(image)
+        x = self.bn1[idx](x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        features = []
+        x, skip = self.forward_resblock(x, self.layer1, idx)
+        features += [skip]
+        x, skip = self.forward_resblock(x, self.layer2, idx)
+        features += [skip]
+        x, skip = self.forward_resblock(x, self.layer3, idx)
+        features += [skip]
+        x, skip = self.forward_resblock(x, self.layer4, idx)
+        features += [skip]
+
+        skip_feats = [b(f) for b, f in zip(self.upsample_bottlenecks, reversed(features))]
+
+        for i, s in enumerate(reversed(skip_feats)):
+            skips[idx + i] += [s]
+
+        return skips
+
+    def forward(self, image):
+        # if isinstance(self.bn1[0], nn.BatchNorm2d):
+        #     if hasattr(self, 'img_scale'):
+        #         image /= self.img_scale
+        #     image -= self.img_mean
+        #     image /= self.img_std
+        pyramid = [image]
+        for l in range(1, self.pyramid_levels):
+            if self.target_size is not None:
+                ts = list([si // 2 ** l for si in self.target_size])
+                pyramid += [
+                    F.interpolate(image, size=ts, mode=self.pyramid_subsample, align_corners=self.align_corners)]
+            else:
+                pyramid += [F.interpolate(image, scale_factor=1 / 2 ** l, mode=self.pyramid_subsample,
+                                          align_corners=self.align_corners)]
+        skips = [[] for _ in range(self.num_skip_levels)]
+        additional = {'pyramid': pyramid}
+        for idx, p in enumerate(pyramid):
+            skips = self.forward_down(p, skips, idx=idx)
+        skips = skips[::-1]
+        x = skips[0][0]
+        if self.detach_upsample_in:
+            x = x.detach()
+        for i, (sk, blend) in enumerate(zip(skips[1:], self.upsample_blends)):
+            x = blend(x, sum(sk))
+        return x, additional
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        super(ResNet, self)._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys,
+                                                  unexpected_keys, error_msgs)
+        for bn in self.bn1:
+            bn._load_from_state_dict(state_dict, prefix + 'bn1.', local_metadata, strict, missing_keys, unexpected_keys,
+                                     error_msgs)
 
 def resnet18(pretrained=True, **kwargs):
     """Constructs a ResNet-18 model.
@@ -269,6 +568,15 @@ def resnet18(pretrained=True, **kwargs):
         model.load_state_dict(model_zoo.load_url(model_urls['resnet18']), strict=False)
     return model
 
+def resnet18_mulbn(pretrained=True, **kwargs):
+    """Constructs a mul-bn ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_mulbn(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']), strict=False)
+    return model
 
 def resnet34(pretrained=True, **kwargs):
     """Constructs a ResNet-34 model.
